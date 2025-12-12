37a06d361b6e800131d66551c21a840e31ec5161
Zacheus <segunzacheusi@gmail.com>
2025-12-12 03:54:58 +0100

    chore(test): add devnet accounts and vitest clarinet test config

---
+++ vitest.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { vitestSetupFilePath, getClarinetVitestsArgv } from "@hirosystems/clarinet-sdk/vitest";
+
+export default defineConfig({
+  test: {
+    environment: "clarinet",
+    pool: "forks",
+    poolOptions: {
+      threads: { singleThread: true },
+      forks: { singleFork: true },
+    },
+    setupFiles: [vitestSetupFilePath],
+    environmentOptions: {
+      clarinet: {
+        ...getClarinetVitestsArgv(),
+      },
+    },
+  },
+});
